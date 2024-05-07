527efe21588d7b4dffe7aafb2a488aeb62bd1df0
camilo-camargo <camilocamargo49@gmail.com>
2024-05-07 17:32:19 -0500

    refactor: refactor register with layouts

--- web/app/routes/register.tsx
+++ web/app/routes/register.tsx
@@ -1,10 +1,11 @@
 import { ActionFunctionArgs } from "@remix-run/node";
 import { Link, useFetcher, useNavigate } from "@remix-run/react";
 import { useRef } from "react";
-import { Logo } from "~/components/assets/Logo";
 import { Button } from "~/components/core/Button";
 import { TextInput } from "~/components/core/TextInput";
 import { apiPost } from "~/services/api";
+import { MainLayout } from "./components/layouts/MainLayout";
+import { ImageSectionLayout } from "./components/layouts/ImageSectionLayout";
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
@@ -20,30 +21,20 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function Route() {
-  const imageLink = "https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+  const navigate = useNavigate();
 
   const fetcher = useFetcher();
   const formRef = useRef<HTMLFormElement>(null);
-  const navigate = useNavigate();
+
+  const imageLink = "https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 
   return (
-    <div className="flex flex-col">
-
-      <nav className="flex p-4 px-32 border-b border-black">
-        <Logo onClick={() => navigate('/')} />
-      </nav>
-
-      <section className="flex w-full">
-        <div className="flex w-2/3">
-          <img className="min-h-full w-auto" src={imageLink} />
-        </div>
-
-        <div className="flex flex-col w-1/3 py-10 px-12 gap-10">
-          <div className="flex flex-col">
-            <h2 className="font-bold text-4xl">Register</h2>
-            <span className="font-light">Register to our system</span>
-          </div>
-
+    <MainLayout>
+      <ImageSectionLayout imageUrl={imageLink} header={{
+        title: "Register",
+        phrase: "Register to our system"
+      }}>
+        <div className="flex flex-col gap-4">
           <fetcher.Form method="POST" ref={formRef} className="flex w-full flex-col justify-center items-center gap-4">
             <TextInput name="givenName" label="Given Name" placeholder="Example" />
             <TextInput name="surname" label="Surname" placeholder="Example" />
@@ -52,23 +43,24 @@ export default function Route() {
             <TextInput name="retryPassword" label="Retry password" placeholder="*****" />
           </fetcher.Form>
 
-          <Button onClick={() => {
-            formRef.current?.submit();
-          }} primary value="Register" />
-          <Button value="Sign In" />
+          <div className="flex flex-col gap-2">
+            <Button onClick={() => {
+              formRef.current?.submit();
+            }} primary value="Register" />
+            <Button
+              value="Sign In"
+              onClick={() => {
+                navigate('/sign-in');
+              }} />
+          </div>
 
 
           <div className="flex gap-2">
             <span>Already have an account?</span>
-            <Link to="sign-in">Sign In</Link>
+            <Link to="/sign-in">Sign In</Link>
           </div>
-
         </div>
-      </section>
-
-      <footer className="flex h-16 px-32 bg-black justify-center items-center">
-        <span className="font-light text-white">{`© ${(new Date().getFullYear())} microBooking`}</span>
-      </footer>
-    </div>
+      </ImageSectionLayout>
+    </MainLayout>
   );
 }
